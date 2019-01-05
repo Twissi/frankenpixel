2dc7333c4c0b8e4e4c07798247f5b517953545d4
Kristin Fritsch <ich@kristin-fritsch.de>
2019-01-05 17:31:47 +0100

    Reformat

--- src/components/Bodypart.js
+++ src/components/Bodypart.js
@@ -3,19 +3,18 @@ import { parts } from '../parts';
 
 export default class BodyPart extends Phaser.GameObjects.Image {
   constructor({ scene, part }) {
-
     if (parts[part] === parts.ARM) {
-        super(scene, 100,75, 'arm');
-     }
-     if (parts[part] === parts.LEG) {
-        super(scene, 75,75, 'leg');
-     }
-     if (parts[part] === parts.MOUTH) {
-        super(scene, 50,25, 'mouth');
-     }
-     if (parts[part] === parts.EYE) {
-        super(scene, 25,25, 'eye');
-     }
+      super(scene, 100, 75, 'arm');
+    }
+    if (parts[part] === parts.LEG) {
+      super(scene, 75, 75, 'leg');
+    }
+    if (parts[part] === parts.MOUTH) {
+      super(scene, 50, 25, 'mouth');
+    }
+    if (parts[part] === parts.EYE) {
+      super(scene, 25, 25, 'eye');
+    }
 
     if (!part) {
       throw Error('Part type is missing or does not exist');
@@ -27,6 +26,4 @@ export default class BodyPart extends Phaser.GameObjects.Image {
   pauseMovement() {
     this.body.setVelocity(0);
   }
-
-
 }
